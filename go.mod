f5c7c61a633c27802ae6b5e4731feb3f7370833a
Abdul Salam <abdullateef@google.com>
2022-09-10 04:25:12 +0000

    added server

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.1.0 // indirect
 	github.com/googleapis/gax-go/v2 v2.5.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
 	go.opencensus.io v0.23.0 // indirect
 	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
 	golang.org/x/oauth2 v0.0.0-20220822191816-0ebed06d0094 // indirect
--- main.go
+++ main.go
@@ -1,69 +1,196 @@
 package main
 
 import (
-	// "bytes"
-	"time"
 	"context"
-	// "flag"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
-	"encoding/json"
+	"os/signal"
+	"strconv"
+	"time"
 
 	reservation "cloud.google.com/go/bigquery/reservation/apiv1"
 	cloudtasks "cloud.google.com/go/cloudtasks/apiv2beta3"
+	"cloud.google.com/go/compute/metadata"
+	"github.com/gorilla/mux"
 	"google.golang.org/api/iterator"
 	reservationpb "google.golang.org/genproto/googleapis/cloud/bigquery/reservation/v1"
 	taskspb "google.golang.org/genproto/googleapis/cloud/tasks/v2beta3"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-var (
-	adminProjectID  = "data-demo-269919"
-	region          = "US"
-	extraSlot       = int64(100)
-	maxSlots        = int64(500)
-	deleteCommitURL = "/del_capacity"
-	queue           = "commit-delete-queue"
-	queueRegion = "us-east1"
-	minutes = 100
+const (
+	addCapacityPath    = "/add_capacity"
+	deleteCapacityPath = "/del_capacity"
+
+	defaultRegion      = "US"
+	defaultMinute      = int64(1)
+
 )
 
+var (
+	maxSlots           int64
+	queue, queueRegion string
+	port, projectID string
+)
+
+// ENV config
+type Config struct {
+	MaxSlot     int64
+	QueueID     string
+	QueueRegion string
+}
+
+func init() {
+	var err error
+	// Run from BigQuery Admin project
+	if projectID = os.Getenv("GOOGLE_CLOUD_PROJECT"); projectID == "" {
+		projectID, err = metadata.ProjectID()
+		if err != nil {
+			log.Fatalf("projectID is not provided")
+		}
+	}
+
+	if port = os.Getenv("PORT"); port == "" {
+		port = "8080"
+	}
+
+	if maxSlots, err = strconv.ParseInt(os.Getenv("MAX_SLOTS"), 10, 64); err != nil {
+		log.Fatal("error: cannot parse MAX_SLOTS")
+	} else if maxSlots <= 0 {
+		log.Fatal("MAX_SLOTS can not be less than or equal to zero.")
+	}
+
+	if queue = os.Getenv("QUEUE"); queue == "" {
+		log.Fatal("QUEUE can not be empty. Create and provide a queue id")
+	}
+
+	if queueRegion = os.Getenv("QUEUE_REGION"); queueRegion == "" {
+		log.Fatal("QUEUE_REGION can not be empty. Provide queue region")
+	}
+}
+
 func main() {
-	ctx := context.Background()
-	commit, err := addCapacity(ctx, adminProjectID, region, extraSlot, maxSlots)
+	r := mux.NewRouter()
+	r.HandleFunc(addCapacityPath, addCapacityHandler).Methods("POST")
+	r.HandleFunc(deleteCapacityPath, deleteCapacityHandler).Methods("POST")
+	r.HandleFunc("/healthz", healthzHandler).Methods("GET")
+
+	srv := &http.Server{
+		Handler: r,
+		Addr:    ":" + port,
+
+		WriteTimeout: 60 * time.Second,
+		ReadTimeout:  30 * time.Second,
+		IdleTimeout:  60 * time.Second,
+	}
+
+	go func() {
+		log.Printf("starting server on port %s", port)
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel()
+
+	srv.Shutdown(ctx)
+
+	log.Println("shutting down")
+	os.Exit(0)
+}
+
+// HTTP request payload for adding capacity
+type Payload struct {
+	Minutes   int64  `json:"minutes"`
+	Region    string `json:"region"`
+	ExtraSlot int64  `json:"extra_slot"`
+}
+
+func addCapacityHandler(w http.ResponseWriter, r *http.Request) {
+	var p Payload
+	if err := json.NewDecoder(r.Body).Decode(&p); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "errors: %v", err)
+		return
+	}
+	defer r.Body.Close()
+
+	if p.Region == "" {
+		p.Region = defaultRegion
+	}
+	if p.Minutes <= 0 {
+		p.Minutes = defaultMinute
+	}
+	if p.ExtraSlot == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "errors: required extraslot not provided")
+		return
+	}
+
+	commit, err := addCapacity(r.Context(), projectID, p.Region, p.ExtraSlot, maxSlots)
 	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "errors: %v", err)
+
 		log.Println(err)
+		return
 	}
 
 	if commit != nil {
 		log.Printf("purchased commitmment, launching delete task for commit ID: %s", commit.Name)
-		if err := launchDeleteTask(ctx, adminProjectID, queueRegion, queue, commit.Name, minutes); err != nil {
+		if err := launchDeleteTask(r.Context(), r, projectID, queueRegion, queue, commit.Name, p.Minutes); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "errors: %v", err)
+
 			log.Println(err)
+			return
 		}
 	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"data":"request processed"}"`))
+}
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, `{"status":ok}`)
+	fmt.Fprintf(w, "\n")
 }
 
 func addCapacity(ctx context.Context, adminProjectID, region string, extraSlot, maxSlots int64) (*reservationpb.CapacityCommitment, error) {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
+
 	client, err := reservation.NewClient(ctx)
 	if err != nil {
 		return nil, err
 	}
 	defer client.Close()
-	parentArg := fmt.Sprintf("projects/%s/locations/%s", adminProjectID, region)
 
-	slotsToAdd := checkProjectSlots(ctx, client, parentArg, extraSlot, maxSlots)
+	parent := fmt.Sprintf("projects/%s/locations/%s", adminProjectID, region)
 
+	slotsToAdd, err := checkProjectSlots(ctx, client, parent, extraSlot, maxSlots)
+	if err != nil {
+		return nil, fmt.Errorf("getting project slots: %v", err)
+	}
 	if slotsToAdd <= 0 {
 		return nil, errors.New("slots to add to capacity can not be less than or equal to zero OR commitment has reached MAX Capacity Slot")
 	}
 
 	req := &reservationpb.CreateCapacityCommitmentRequest{
 		// See https://pkg.go.dev/google.golang.org/genproto/googleapis/cloud/bigquery/reservation/v1#CreateCapacityCommitmentRequest.
-		Parent: parentArg,
+		Parent: parent,
 		CapacityCommitment: &reservationpb.CapacityCommitment{
 			SlotCount: slotsToAdd,
 			Plan:      reservationpb.CapacityCommitment_FLEX,
@@ -71,13 +198,13 @@ func addCapacity(ctx context.Context, adminProjectID, region string, extraSlot, 
 	}
 	resp, err := client.CreateCapacityCommitment(ctx, req)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("creating capacity commitment: %v", err)
 	}
 
 	return resp, nil
 }
 
-func checkProjectSlots(ctx context.Context, client *reservation.Client, parent string, extraSlots, maxSlots int64) int64 {
+func checkProjectSlots(ctx context.Context, client *reservation.Client, parent string, extraSlots, maxSlots int64) (int64, error) {
 	var total int64
 	req := &reservationpb.ListCapacityCommitmentsRequest{
 		// See https://pkg.go.dev/google.golang.org/genproto/googleapis/cloud/bigquery/reservation/v1#ListCapacityCommitmentsRequest.
@@ -90,35 +217,30 @@ func checkProjectSlots(ctx context.Context, client *reservation.Client, parent s
 			break
 		}
 		if err != nil {
-			log.Println(err)
-			return 0
+			return 0, err
 		}
-		// TODO: Use resp.
 		total = total + resp.SlotCount
 	}
 
 	slotCap := maxSlots - total
-	return min(extraSlots, slotCap)
+	return min(extraSlots, slotCap), nil
 }
 
+// Commit request for deleteCapacity
 type Commit struct {
 	CommitID string `json:"commit_id"`
 }
 
-func launchDeleteTask(ctx context.Context, adminProjectID, queueRegion, queue, commitName string, minutes int) error {
-	host, err := os.Hostname()
-	if err != nil {
-		log.Fatal(err)
-	}
+func launchDeleteTask(ctx context.Context, r *http.Request, adminProjectID, queueRegion, queue, commitName string, minutes int64) error {
+	host := r.Host
 
-	deleteCommitURL = "https://" + host + deleteCommitURL
-	log.Println(deleteCommitURL)
+	deleteURL := host + deleteCapacityPath
+	log.Println(deleteURL)
 
 	c, err := cloudtasks.NewClient(ctx)
 	if err != nil {
 		return err
 	}
-
 	defer c.Close()
 
 	body, err := json.Marshal(Commit{CommitID: commitName})
@@ -128,13 +250,12 @@ func launchDeleteTask(ctx context.Context, adminProjectID, queueRegion, queue, c
 
 	taskTime := time.Now().Add(time.Duration(minutes) * time.Minute)
 	req := &taskspb.CreateTaskRequest{
-		// TODO: Fill request struct fields.
 		// See https://pkg.go.dev/google.golang.org/genproto/googleapis/cloud/tasks/v2beta3#CreateTaskRequest.
 		Parent: fmt.Sprintf("projects/%s/locations/%s/queues/%s", adminProjectID, queueRegion, queue),
 		Task: &taskspb.Task{
 			PayloadType: &taskspb.Task_HttpRequest{
 				HttpRequest: &taskspb.HttpRequest{
-					Url:        deleteCommitURL,
+					Url:        deleteURL,
 					HttpMethod: taskspb.HttpMethod_POST,
 					Body:       body,
 					Headers: map[string]string{
@@ -149,9 +270,38 @@ func launchDeleteTask(ctx context.Context, adminProjectID, queueRegion, queue, c
 	if err != nil {
 		return err
 	}
-	
+
 	log.Printf("delete commitment task created %s", resp.Name)
 	return nil
+}
+
+
+func deleteCapacityHandler(w http.ResponseWriter, r *http.Request) {
+	var c Commit
+	if err := json.NewDecoder(r.Body).Decode(&c); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "errors: %v", err)
+		return
+	}
+	defer r.Body.Close()
+
+	if c.CommitID == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "errors: required CommitID not provided")
+		return
+	}
+
+	if err := deleteCapacity(r.Context(), c.CommitID); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "errors: %v", err)
+
+		log.Println(err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"data":"request processed"}"`))
 }
 
 func deleteCapacity(ctx context.Context, commitName string) error {
@@ -166,7 +316,7 @@ func deleteCapacity(ctx context.Context, commitName string) error {
 
 	req := &reservationpb.DeleteCapacityCommitmentRequest{
 		// See https://pkg.go.dev/google.golang.org/genproto/googleapis/cloud/bigquery/reservation/v1#DeleteCapacityCommitmentRequest.
-		Name:commitName,
+		Name:  commitName,
 		Force: false,
 	}
 
